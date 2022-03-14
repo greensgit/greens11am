9c349d59d75d1f6b0533529a3dd2b5ec187ba6f4
greensgit <rajgreensomr@gmail.com>
2022-03-13 21:34:56 -0700

    new code

---
+++ src/org/cts/java/Employee.java
@@ -0,0 +1,26 @@
+package org.cts.java;
+
+//AccessSpacifier class ClassName
+public class Employee {
+	
+	private void empId() {//ctrl+Space,Enter
+		
+		System.out.println("Employee id is 6543");//sysout,ctrl+space
+	}
+	private void empName() {
+
+		System.out.println("Employee name is Deva");
+	}
+	
+public static void main(String[] args) {
+	
+//	object creation
+//	ClassName ObjectName =new ClassName();
+	Employee a = new Employee();
+//	method Calling
+//	ObjectName.methodName();
+	a.empId();
+	a.empName();
+	
+}
+}
